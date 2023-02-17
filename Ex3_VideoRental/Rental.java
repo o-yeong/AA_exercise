6f193af1eaea039d9b383709acdc6e3ca78b6cf1
o-yeong <aimar.gwon@gmail.com>
2023-02-17 16:16:56 +0900

    Update Rental.java

--- Ex3_VideoRental/Rental.java
+++ Ex3_VideoRental/Rental.java
@@ -60,11 +60,7 @@ public class Rental {
 		}
 		if ( daysRented <= 2) return limit ;
 
-		switch ( video.getVideoType() ) {
-			case Video.VHS: limit = 5 ; break ;
-			case Video.CD: limit = 3 ; break ;
-			case Video.DVD: limit = 2 ; break ;
-		}
-		return limit ;
+		
+		return return video.getVideoType().getLimit(); ;
 	}
 }
